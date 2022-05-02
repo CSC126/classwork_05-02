f278e7cad509f555f0926c41543ec91a103f2a54
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-05-02 13:28:09 -0400

    kinda working?

--- classwork_05-02/main.cpp
+++ classwork_05-02/main.cpp
@@ -1,14 +1,41 @@
-//
-//  main.cpp
-//  classwork_05-02
-//
-//  Created by Maxim Voyevoda on 5/2/22.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    int alpha[50] = {0};
+    
+//    alpha[0] = 1;
+    
+//    for (int i = 1; i < 20; i++){
+//        alpha[i] = i + 2;
+//        cout << alpha[i] << " ";
+//    }
+//
+//    alpha[20] = 22;
+//
+//    for (int i = 21; i < 50; i++){
+//        alpha[i] = i + 2;
+//        cout << alpha[i] << " ";
+//    }
+//
+    
+    alpha[0] = 1;
+    cout << alpha[0] << " ";
+    
+    for (int i = 1; i < 50; i++){
+        
+        if (i == 20){
+            alpha[i] = alpha[i-1] + 1;
+            cout << "Break ";
+        } else {
+            alpha[i] = alpha[i-1] + 2;
+            cout << alpha[i] << " ";
+        }
+        
+    }
+    
+    
+    cout << endl << endl;
     return 0;
 }
